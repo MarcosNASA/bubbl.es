639243b88ac06a5cb00521df01778937589a2135
Marc <marc@taxscouts.com>
2021-09-07 20:50:16 +0200

    Fix bubble's cursor on hover

--- src/components/ui/Bubbles.js
+++ src/components/ui/Bubbles.js
@@ -11,10 +11,9 @@ export const Bubble = styled(motion.span).attrs({
     `
       position: relative;
       z-index: 1;
-      border: 1px solid ${colors.dark[200]}
 
       :hover {
-          cursor: help;
+        cursor: default;
       }
 
       :after {
@@ -28,8 +27,8 @@ export const Bubble = styled(motion.span).attrs({
           z-index: -1;
           padding: 0px 2px ${$isFormalParameterOrIteratorAble ? 6 : 2}px 2px;
           border-radius: 3px;
-        }
-    `}
+      }
+  `}
 `
 
 const GLIPH_WIDTH = 9.61
@@ -52,19 +51,19 @@ export const ScopeBubble = styled(motion.div).attrs({
       }
 
       :after {
-          content: '';
-          background: ${background};
-          position: absolute;
-          top: -6px;
-          left: ${!level ? 0 : Math.max(0, level - 2) * INDENT_SIZE + 16}px;
-          height: 100%;
-          width: ${!level ? `100%` : `calc(100% - ${(level - 2) * INDENT_SIZE + 16}px)`};
-          z-index: -1;
-          padding: 4px 4px 6px 0px;
-          padding-right: ${
-            !level ? (levels + 1) * (GLIPH_WIDTH * 2) + 16 : (Math.max(0, levels - level) * INDENT_SIZE + 16) / 2
-          }px;
-          border-radius: 3px;
+        content: '';
+        background: ${background};
+        position: absolute;
+        top: -6px;
+        left: ${!level ? 0 : Math.max(0, level - 2) * INDENT_SIZE + 16}px;
+        height: 100%;
+        width: ${!level ? `100%` : `calc(100% - ${(level - 2) * INDENT_SIZE + 16}px)`};
+        z-index: -1;
+        padding: 4px 4px 6px 0px;
+        padding-right: ${
+          !level ? (levels + 1) * (GLIPH_WIDTH * 2) + 16 : (Math.max(0, levels - level) * INDENT_SIZE + 16) / 2
+        }px;
+        border-radius: 3px;
       }
-    `}
+  `}
 `
